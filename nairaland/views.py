4bdb624b359e6186b4038c6fd6ff0f9a259325a2
Brodaoj <olujas1@gmail.com>
2021-07-10 17:54:23 +0100

    CRUD task, first commit

---
+++ nairaland/views.py
@@ -0,0 +1,33 @@
+from django.views.generic import ListView , DetailView
+from django.views.generic.edit import CreateView , UpdateView , DeleteView
+from .models import post
+from django.urls import reverse_lazy
+
+class BlogListView(ListView):
+    model = post
+    template_name = 'home.html'
+
+
+class BlogDetailView(DetailView):
+    model = post
+    template_name = 'post_detail.html'   
+
+
+class BlogCreateView(CreateView): 
+    model = post
+    template_name = 'post_new.html'
+    fields = ['title', 'author', 'body']
+
+
+class BlogUpdateView(UpdateView):
+    model = post
+    template_name = 'post_edit.html'
+    fields = ['title', 'body']
+    
+
+class BlogDeleteView(DeleteView):
+    model = post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
+    
+     
